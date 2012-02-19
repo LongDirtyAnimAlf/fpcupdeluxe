0a192b8dce8d48e5e136ed16fbf3b73751f74d2d
reiniero <reiniero@localhost>
2012-02-19 17:51:06 +0100

    moved code installer=>fpcuputil

--- installer.pas
+++ installer.pas
@@ -82,13 +82,9 @@ type
     FVerbose: boolean;
     function CheckExecutable(Executable, Parameters, ExpectOutput: string): boolean;
     procedure CreateBinutilsList;
-    procedure CreateDesktopShortCut(Target, TargetArguments, ShortcutName: string) ;
-    procedure CreateHomeStartLink(Target, TargetArguments, ShortcutName: string);
     function DownloadBinUtils: boolean;
     function DownloadBootstrapCompiler: boolean;
     function DownloadFPCHelp(URL, TargetDirectory: string): boolean;
-    function DownloadFTP(URL, TargetFile: string): boolean;
-    function DownloadHTTP(URL, TargetFile: string): boolean;
     function DownloadSVN: boolean;
     procedure DumpOutput(Sender:TProcessEx; output:string);
     function CheckAndGetNeededExecutables: boolean;
@@ -105,8 +101,6 @@ type
     function GetLazarusRevision: string;
     procedure LogError(Sender:TProcessEx;IsException:boolean);
     function ModuleEnabled(Name:string):boolean;
-    function MoveFile(const SrcFilename, DestFilename: string): boolean;
-    // Moves file if it exists, overwriting destination file
     procedure SetAllOptions(AValue: string);
     procedure SetCrossCPU_Target(AValue: string);
     procedure SetCrossOS_Target(AValue: string);
@@ -120,7 +114,6 @@ type
     procedure SetSkipLazarusHelp(AValue: boolean);
     procedure SetSkipModules(AValue: string);
     procedure SetVerbose(AValue: boolean);
-    function Which(Executable: string): string; //Runs which command. Returns full path of executable, if it exists
     function GetLazarusDirectory: string;
     function GetLazarusUrl: string;
     function GetMakePath: string;
@@ -133,9 +126,6 @@ type
     procedure SetLazarusOPT(AValue: string);
     procedure SetLazarusUrl(AValue: string);
     procedure SetMakePath(AValue: string);
-    {$IFDEF UNIX}
-    function XdgConfigHome: String;
-    {$ENDIF UNIX}
   public
     property ShortCutName: string read FShortcutName write FShortcutName; //Name of the shortcut to Lazarus. If empty, no shortcut is generated.
     property ShortCutNameFpcup:string read FShortCutNameFpcup write SetShortCutNameFpcup;
@@ -187,117 +177,14 @@ type
     destructor Destroy; override;
   end;
 
-procedure infoln(Message: string);
-//Uses writeln for now, and waits a bit afterwards so output is hopefully not garbled
-
 implementation
 
 uses
-  httpsend {for downloading from http},
-  ftpsend {for downloading from ftp},
-  strutils, process, FileUtil {Requires LCL}
-{$IFDEF MSWINDOWS}
-  //Mostly for shortcut code
-  ,windows, shlobj {for special folders}, ActiveX, ComObj
-{$ENDIF MSWINDOWS}
+  strutils, {process,} FileUtil {Requires LCL}
 {$IFDEF UNIX}
   ,baseunix
 {$ENDIF UNIX}
-  ,updatelazconfig, fpcuputil
-  ;
-
-procedure infoln(Message: string);
-begin
-  {DEBUG conditional symbol is defined using
-  Project Options/Other/Custom Options using -dDEBUG
-  }
-  //$IFDEF DEBUG //ignore for now
-  writeln('Info: ' + Message); //we misuse this for info output
-  sleep(200); //hopefully allow output to be written without interfering with other output
-  //$ENDIF DEBUG
-end;
-
-{$IFDEF UNIX}
-//Adapted from sysutils; Unix/Linux only
-Function TInstaller.XdgConfigHome: String;
-{ Follows base-dir spec,
-  see [http://freedesktop.org/Standards/basedir-spec].
-  Always ends with PathDelim. }
-begin
-  Result:=GetEnvironmentVariable('XDG_CONFIG_HOME');
-  if (Result='') then
-    Result:=IncludeTrailingPathDelimiter(ExpandFileNameUTF8('~'))+'.config'+DirectorySeparator
-  else
-    Result:=IncludeTrailingPathDelimiter(Result);
-end;
-{$ENDIF UNIX}
-
-{$IFDEF MSWINDOWS}
-procedure TInstaller.CreateDesktopShortCut(Target, TargetArguments, ShortcutName: string);
-var
-  IObject: IUnknown;
-  ISLink: IShellLink;
-  IPFile: IPersistFile;
-  PIDL: PItemIDList;
-  InFolder: array[0..MAX_PATH] of Char;
-  LinkName: WideString;
-begin
-  { Creates an instance of IShellLink }
-  IObject := CreateComObject(CLSID_ShellLink);
-  ISLink := IObject as IShellLink;
-  IPFile := IObject as IPersistFile;
-
-  ISLink.SetPath(pChar(Target));
-  ISLink.SetArguments(pChar(TargetArguments));
-  ISLink.SetWorkingDirectory(pChar(ExtractFilePath(Target)));
-
-  { Get the desktop location }
-  SHGetSpecialFolderLocation(0, CSIDL_DESKTOPDIRECTORY, PIDL);
-  SHGetPathFromIDList(PIDL, InFolder);
-  LinkName := InFolder + PathDelim + ShortcutName+'.lnk';
-
-  { Get rid of any existing shortcut first }
-  SysUtils.DeleteFile(LinkName);
-
-  { Create the link }
-  IPFile.Save(PWChar(LinkName), false);
-end;
-{$ENDIF MSWINDOWS}
-
-{$IFDEF UNIX}
-procedure TInstaller.CreateDesktopShortCut(Target, TargetArguments, ShortcutName: string);
-begin
-  infoln('todo: implement createdesktopshortcut for '+Target+' with '+TargetArguments+' as '+Shortcutname);
-end;
-{$ENDIF UNIX}
-
-procedure TInstaller.CreateHomeStartLink(Target, TargetArguments,
-  ShortcutName: string);
-var
-  ScriptText: TStringList;
-  ScriptFile: string;
-begin
-  {$IFDEF MSWINDOWS}
-  infoln('todo: write me (CreateHomeStartLink)!');
-  {$ENDIF MSWINDOWS}
-  {$IFDEF UNIX}
-  //create dir if it doesn't exist
-  ForceDirectoriesUTF8(ExtractFilePath(IncludeTrailingPathDelimiter(ExpandFileNameUTF8('~'))+ShortcutName));
-  ScriptText:=TStringList.Create;
-  try
-    // No quotes here, either, we're not in a shell, apparently...
-    ScriptFile:=IncludeTrailingPathDelimiter(ExpandFileNameUTF8('~'))+ShortcutName;
-    SysUtils.DeleteFile(ScriptFile); //Get rid of any existing remnants
-    ScriptText.Add('#!/bin/sh');
-    ScriptText.Add('# shortcut to Lazarus trunk, generated by fcpup');
-    ScriptText.Add(Target+' '+TargetArguments);
-    ScriptText.SaveToFile(ScriptFile);
-    FPChmod(ScriptFile, &700); //rwx------
-  finally
-    ScriptText.Free;
-  end;
-  {$ENDIF UNIX}
-end;
+  ,updatelazconfig, fpcuputil;
 
 procedure TInstaller.CreateBinutilsList;
 // Windows-centric for now; doubt if it
@@ -585,160 +472,6 @@ begin
   Result := OperationSucceeded;
 end;
 
-function TInstaller.DownloadFTP(URL, TargetFile: string): boolean;
-const
-  FTPPort=21;
-  FTPScheme='ftp://'; //URI scheme name for FTP URLs
-var
-  Host: string;
-  Port: integer;
-  Source: string;
-  FoundPos: integer;
-begin
-  if LeftStr(URL, length(FTPScheme))=FTPScheme then URL:=Copy(URL, length(FTPScheme)+1, length(URL));
-  FoundPos:=pos('/', URL);
-  Host:=LeftStr(URL, FoundPos-1);
-  Source:=Copy(URL, FoundPos+1, Length(URL));
-  //Check for port numbers:
-  FoundPos:=pos(':', Host);
-  Port:=FTPPort;
-  if FoundPos>0 then
-  begin
-    Host:=LeftStr(Host, FoundPos-1);
-    Port:=StrToIntDef(Copy(Host, FoundPos+1, Length(Host)),21);
-  end;
-  Result:=FtpGetFile(Host, IntToStr(Port), Source, TargetFile, 'anonymous', 'fpc@example.com');
-  if result=false then infoln('DownloadFTP: error downloading '+URL+'. Details: host: '+Host+'; port: '+Inttostr(Port)+'; remote path: '+Source+' to '+TargetFile);
-end;
-
-function TInstaller.DownloadHTTP(URL, TargetFile: string): boolean;
-  // Download file. If ncessary deal with SourceForge redirection, thanks to
-  // Ocye: http://lazarus.freepascal.org/index.php/topic,13425.msg70575.html#msg70575
-const
-  SFProjectPart = '//sourceforge.net/projects/';
-  SFFilesPart = '/files/';
-  SFDownloadPart ='/download';
-  MaxRetries = 3;
-var
-  Buffer: TMemoryStream;
-  HTTPGetResult: boolean;
-  i, j: integer;
-  HTTPSender: THTTPSend;
-  IsSFDownload: boolean;
-  SFProjectBegin: integer;
-  RetryAttempt: integer;
-  SFDirectory: string; //Sourceforge directory
-  SFDirectoryBegin: integer;
-  SFFileBegin: integer;
-  SFFilename: string; //Sourceforge name of file
-  SFProject: string;
-begin
-  Result := False;
-  IsSFDownload:=false;
-
-  // Detect SourceForge download; e.g. from URL
-  //          1         2         3         4         5         6         7         8         9
-  // 1234557890123456789012345578901234567890123455789012345678901234557890123456789012345578901234567890
-  // http://sourceforge.net/projects/base64decoder/files/base64decoder/version%202.0/b64util.zip/download
-  //                                 ^^^project^^^       ^^^directory............^^^ ^^^file^^^
-  i:=Pos(SFProjectPart, URL);
-  if i>0 then
-  begin
-    // Possibly found project; now extract project, directory and filename parts.
-    SFProjectBegin:=i+Length(SFProjectPart);
-    j := PosEx(SFFilesPart, URL, SFProjectBegin);
-    if (j>0) then
-    begin
-      SFProject:=Copy(URL, SFProjectBegin, j-SFProjectBegin);
-      SFDirectoryBegin:=PosEx(SFFilesPart, URL, SFProjectBegin)+Length(SFFilesPart);
-      if SFDirectoryBegin>0 then
-      begin
-        // Find file
-        // URL might have trailing arguments... so: search for first
-        // /download coming up from the right, but it should be after
-        // /files/
-        i:=RPos(SFDownloadPart, URL);
-        // Now look for previous / so we can make out the file
-        // This might perhaps be the trailing / in /files/
-        SFFileBegin:=RPosEx('/',URL,i-1)+1;
-
-        if SFFileBegin>0 then
-        begin
-          SFFilename:=Copy(URL,SFFileBegin, i-SFFileBegin);
-          //Include trailing /
-          SFDirectory:=Copy(URL, SFDirectoryBegin, SFFileBegin-SFDirectoryBegin);
-          IsSFDownload:=true;
-        end;
-      end;
-    end;
-  end;
-
-  if IsSFDownload then
-  begin
-    try
-      // Rewrite URL if needed for Sourceforge download redirection
-      HTTPSender := THTTPSend.Create;
-      while not Result do
-      begin
-        HTTPSender.HTTPMethod('GET', URL);
-        infoln('debug: headers:');
-        infoln(HTTPSender.Headers.Text);
-        case HTTPSender.Resultcode of
-          301, 302, 307: for i := 0 to HTTPSender.Headers.Count - 1 do
-              if (Pos('Location: ', HTTPSender.Headers.Strings[i]) > 0) or
-                (Pos('location: ', HTTPSender.Headers.Strings[i]) > 0) then
-              begin
-                j := Pos('use_mirror=', HTTPSender.Headers.Strings[i]);
-                if j > 0 then
-                  URL :=
-                    'http://' + RightStr(HTTPSender.Headers.Strings[i],
-                    length(HTTPSender.Headers.Strings[i]) - j - 10) +
-                    '.dl.sourceforge.net/project/' +
-                    SFProject + '/' + SFDirectory + SFFilename
-                else
-                  URl :=
-                    StringReplace(HTTPSender.Headers.Strings[i], 'Location: ', '', []);
-                HTTPSender.Clear;//httpsend
-                break;
-              end;
-          100..200: Result := True; //No changes necessary
-          500: raise Exception.Create('No internet connection available');
-            //Internal Server Error ('+aURL+')');
-          else
-            raise Exception.Create('Download failed with error code ' +
-              IntToStr(HTTPSender.ResultCode) + ' (' + HTTPSender.ResultString + ')');
-        end;//case
-      end;//while
-      infoln('debug: resulting url after sf redir: *' + URL + '*');
-    finally
-      HTTPSender.Free;
-    end;
-  end;
-
-  try
-    Buffer := TMemoryStream.Create;
-    infoln('Going to call httpgetbinary for url: ' + URL);
-    RetryAttempt := 1;
-    HTTPGetResult := False;
-    while ((HTTPGetResult = False) and (RetryAttempt < MaxRetries)) do
-    begin
-      HTTPGetResult := HttpGetBinary(URL, Buffer);
-      //Application.ProcessMessages;
-      Sleep(100 * RetryAttempt);
-      RetryAttempt := RetryAttempt + 1;
-    end;
-    if HTTPGetResult = False then
-      raise Exception.Create('Cannot load document from remote server');
-    Buffer.Position := 0;
-    if Buffer.Size = 0 then
-      raise Exception.Create('Downloaded document is empty.');
-    Buffer.SaveToFile(TargetFile);
-    Result := True;
-  finally
-    FreeAndNil(Buffer);
-  end;
-end;
-
 function TInstaller.DownloadSVN: boolean;
 var
   OperationSucceeded: boolean;
@@ -1010,26 +743,6 @@ begin
     result:=FInstalledCompilerName
   else
     result:=FBootstrapCompilerName;
-end;
-
-function TInstaller.Which(Executable: string): string;
-var
-  Output: string;
-begin
-  ExecuteCommandHidden('which',Executable,Output,Verbose);
-  //Remove trailing LF(s) and other control codes:
-  while (length(output)>0) and (ord(output[length(output)])<$20) do
-    delete(output,length(output),1);
-  // We could have checked for ExecuteCommandHidden exitcode, but why not
-  // do file existence check instead:
-  if fileexists(Output) then
-  begin
-    result:=Output;
-  end
-  else
-  begin
-    result:=''; //command failed
-  end;
 end;
 
 function Tinstaller.GetFpcDirectory: string;
@@ -1400,25 +1113,6 @@ begin
           or ((FSkipModules<>'') and (Pos(Name,FSkipModules)<=0))
 end;
 
-function TInstaller.MoveFile(const SrcFilename, DestFilename: string): boolean;
-// We might (in theory) be moving files across partitions so we cannot use renamefile
-begin
-  try
-    if FileExistsUTF8(SrcFileName) then
-    begin
-      if FileUtil.CopyFile(SrcFilename, DestFileName) then Sysutils.DeleteFile(SrcFileName);
-      result:=true;
-    end
-    else
-    begin
-      //Source file does not exist, so cannot move
-      result:=false;
-    end;
-  except
-    result:=false;
-  end;
-end;
-
 procedure TInstaller.SetAllOptions(AValue: string);
 begin
   if FAllOptions=AValue then Exit;
@@ -1516,21 +1210,18 @@ procedure TInstaller.SetLazarusOPT(AValue: string);
 begin
   if FLazarusOPT=AValue then Exit;
   FLazarusOPT:=AValue;
-  end;
+end;
+
 procedure TInstaller.SetLazarusPrimaryConfigPath(AValue: string);
 const
   DefaultPCPSubdir='lazarusdevsettings'; //Include the name lazarus for easy searching Caution: shouldn't be the same name as Lazarus dir itself.
-var
-  AppDataPath: array[0..MaxPathLen] of char; //Allocate memory
 begin
   //Directory where Lazarus installation config will end up (primary config path)
   if AValue=EmptyStr then
   begin
     {$IFDEF MSWINDOWS}
     // Somewhere in local appdata special folder
-    AppDataPath := '';
-    SHGetSpecialFolderPath(0, AppDataPath, CSIDL_LOCAL_APPDATA, False);
-    FLazarusPrimaryConfigPath := IncludeTrailingPathDelimiter(AppDataPath)+DefaultPCPSubdir;
+    FLazarusPrimaryConfigPath := IncludeTrailingPathDelimiter(GetLocalAppDataPath())+DefaultPCPSubdir;
     {$ELSE}
     //Note: normsl GetAppConfigDir gets ~/.config/fpcup/.lazarusdev or something
     LazarusPrimaryConfigPath:=IncludeTrailingPathDelimiter(XdgConfigHome)+DefaultPCPSubdir;
