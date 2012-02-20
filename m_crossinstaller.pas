927af23419e3b5041fe2fb486613fa3413c45cb4
reiniero <reiniero@localhost>
2012-02-20 09:57:31 +0100

    Backed out abortive crossinstaller scaffolding (instead using Ludo's) changeset: a7cdf3ba4454

--- m_crossinstaller.pas
+++ m_crossinstaller.pas
@@ -14,178 +14,75 @@ type
   private
   protected
     FBinUtilsPath: string;
-    FBinUtilsPrefix: string;
+    FBinUtilsPrefic: string;
     FLibsPath: string;
     FTargetCPU: string;
     FTargetOS: string;
     procedure SetBinUtilsPath(AValue: string);
-    procedure SetBinUtilsPrefix(AValue: string);
+    procedure SetBinUtilsPrefic(AValue: string);
     procedure SetLibsPath(AValue: string);
     procedure SetTargetCPU(AValue: string);
     procedure SetTargetOS(AValue: string);
+    function GetLibs:boolean;virtual; abstract;
+    function GetBinUtils:boolean;virtual; abstract;
+    property LibsPath:string read FLibsPath write SetLibsPath;
+    property BinUtilsPath:string read FBinUtilsPath write SetBinUtilsPath;
+    property BinUtilsPrefic:string read FBinUtilsPrefic write SetBinUtilsPrefic;
+    property TargetCPU:string read FTargetCPU write SetTargetCPU;
+    property TargetOS:string read FTargetOS write SetTargetOS;
   public
-    function GetLibs:boolean;virtual; abstract; //Used to place required libraries for target system
-    function GetBinUtils:boolean;virtual; abstract; //Used to place required binutils for target system
-    property LibsPath:string read FLibsPath write SetLibsPath; //Returns library path for target system
-    property BinUtilsPath:string read FBinUtilsPath write SetBinUtilsPath; //Returns binutils (make etc) path for target system
-    property BinUtilsPrefix:string read FBinUtilsPrefix write SetBinUtilsPrefix; //Returns prefix used if storing binutils in a common directory
-    property TargetCPU:string read FTargetCPU write SetTargetCPU; //Target CPU according to FPC Makefile format
-    property TargetOS:string read FTargetOS write SetTargetOS; //Target Operating System according to FPC Makefile format
     constructor Create;
     destructor Destroy; override;
   end;
-  TCrossInstallerClass= class of TCrossInstaller;
 
-  { TCrossInstallerDef }
-  // Used to keep information for TCrossInstaller descendants
-  // used in registration
-  TCrossInstallerDef=Class(TPersistent)
-    Class Function CrossInstallerName: String; virtual;
-    Class Function CrossInstallerClass: TCrossInstallerClass; virtual;
-    Class Function TargetCPU: String; virtual;
-    Class Function TargetName: String; virtual;
-  end;
-  TCrossInstallerDefClass = class of TCrossInstallerDef;
-
-  // Used for registering and managing list of possible CrossInstallers via CrossInstallerDefs
-  Procedure RegisterCrossInstaller(Def:TCrossInstallerDefClass);
-  Function GetCrossInstallerDef(CrossInstallerName : String) : TCrossInstallerDef;
-  Procedure GetCrossInstallerList(List : TSTrings);
+Procedure
+RegisterExtension(Platform:string;Extension:TCrossInstaller);
+Var
+  CrossInstallers:TStringList;
 
 implementation
-uses
-  m_crosswin64;
-  //Implementers: add new cross compiler units here
 
 { TCrossInstaller }
-procedure TCrossInstaller.SetBinUtilsPath(AValue: string);
+procedure RegisterExtension(Platform:string;Extension:TCrossInstaller);
 begin
-  raise Exception.Create('Cross compiler extension must implement SetBinUtilsPath');
+  //fill in here
 end;
 
-procedure TCrossInstaller.SetBinUtilsPrefix(AValue: string);
+procedure TCrossInstaller.SetBinUtilsPath(AValue: string);
 begin
-  raise Exception.Create('Cross compiler extension must implement SetBinUtilsPrefix');
+
+end;
+
+procedure TCrossInstaller.SetBinUtilsPrefic(AValue: string);
+begin
+
 end;
 
 procedure TCrossInstaller.SetLibsPath(AValue: string);
 begin
-  raise Exception.Create('Cross compiler extension must implement SetLibsPath');
+
 end;
 
 procedure TCrossInstaller.SetTargetCPU(AValue: string);
 begin
-  raise Exception.Create('Cross compiler extension must implement SetTargetCPU');
+  if FTargetCPU=AValue then Exit;
+  FTargetCPU:=AValue;
 end;
 
 procedure TCrossInstaller.SetTargetOS(AValue: string);
 begin
-  raise Exception.Create('Cross compiler extension must implement SetTargetOS');
+  if FTargetOS=AValue then Exit;
+  FTargetOS:=AValue;
 end;
-
 
 constructor TCrossInstaller.Create;
 begin
-  // Help ensure our implementers do the right thing with the variables
-  // in their extensions
-  FBinUtilsPath:='Error: cross compiler extension must set FBinUtilsPath';
-  FBinUtilsPrefix:='Error: cross compiler extension must set FBinUtilsPrefix';
-  FLibsPath:='Error: cross compiler extension must set FLibsPath';
-  FTargetCPU:='Error: cross compiler extension must set FTargetCPU';
-  FTargetOS:='Error: cross compiler extension must set FTargetOS';
+
 end;
 
 destructor TCrossInstaller.Destroy;
 begin
   inherited Destroy;
-end;
-
-
-{ TCrossInstallerDef }
-class function TCrossInstallerDef.CrossInstallerName: String;
-begin
-  Result:='Error, implement in extension!'; //Must implement in descendant!
-end;
-
-class function TCrossInstallerDef.CrossInstallerClass: TCrossInstallerClass;
-begin
-  Result:=Nil; //Must implement in descendant; use class of <yourinstaller>def
-
-end;
-
-class function TCrossInstallerDef.TargetCPU: String;
-begin
-  Result:='Error, implement in extension!'; //Must implement in descendant; see FPC Makelist for options
-end;
-
-class function TCrossInstallerDef.TargetName: String;
-begin
-  Result:='Error, implement in extension!'; //Must implement in descendant; see FPC Makelist for options
-end;
-
-
-{ CrossInstaller extension list management }
-Var
-  CrossDefs : TStringList;
-
-Procedure CheckDefs;
-begin
-  If (CrossDefs=Nil) then
-    begin
-    CrossDefs:=TStringList.Create;
-    CrossDefs.Sorted:=True;
-    CrossDefs.Duplicates:=dupError;
-    end;
-end;
-
-Procedure DoneDefs;
-var
-  I : Integer;
-begin
-  If Assigned(CrossDefs) then
-    begin
-    For I:=CrossDefs.Count-1 downto 0 do
-      begin
-      CrossDefs.Objects[i].Free;
-      CrossDefs.Delete(I);
-      end;
-    FreeAndNil(CrossDefs);
-    end;
-end;
-
-
-Function GetCrossInstallerDef(CrossInstallerName : String) : TCrossInstallerDef;
-var
-  I : Integer;
-begin
-  CheckDefs;
-  I:=CrossDefs.IndexOf(CrossInstallerName);
-  If (I<>-1) then
-    Result:=TCrossInstallerDef(CrossDefs.Objects[i])
-  else
-    Result:=Nil;
-end;
-
-procedure RegisterCrossInstaller(Def: TCrossInstallerDefClass);
-var
-  I : Integer;
-begin
-  CheckDefs;
-  I:=CrossDefs.IndexOf(Def.CrossInstallerName);
-  If (I=-1) then
-    CrossDefs.AddObject(Def.CrossInstallerName,Def.Create)
-  else
-    begin
-    // Replace existing...
-    CrossDefs.Objects[I].Free;
-    CrossDefs.Objects[I]:=Def.Create;
-    end;
-end;
-
-procedure GetCrossInstallerList(List: TSTrings);
-begin
-  CheckDefs;
-  List.Text:=CrossDefs.Text;
 end;
 
 end.
--- m_crosswin64.pas
+++ m_crosswin64.pas
@@ -7,142 +7,31 @@ interface
 uses
   Classes, SysUtils, m_crossinstaller;
 
-type
-{ TWin32To64 }
-
-  TWin32To64 = class(TCrossInstaller)
-  private
-    // You must set these variables:
-    //FBinUtilsPath: string;
-    //FBinUtilsPrefix: string;
-    //FLibsPath: string;
-    //FTargetCPU: string;
-    //FTargetOS: string;
-    // You must override/replace these
-    // procedures in TCrossInstaller.
-    procedure SetBinUtilsPath(AValue: string);
-    procedure SetBinUtilsPrefix(AValue: string);
-    procedure SetLibsPath(AValue: string);
-    procedure SetTargetCPU(AValue: string);
-    procedure SetTargetOS(AValue: string);
-  public
-    // You must override GetLibs and GetBinUtils
-    function GetLibs:boolean;override;
-    function GetBinUtils:boolean;override;
-    property LibsPath:string read FLibsPath write SetLibsPath;
-    property BinUtilsPath:string read FBinUtilsPath write SetBinUtilsPath;
-    property BinUtilsPrefix:string read FBinUtilsPrefix write SetBinUtilsPrefix;
-    property TargetCPU:string read FTargetCPU write SetTargetCPU;
-    property TargetOS:string read FTargetOS write SetTargetOS;
-    constructor Create;
-    destructor Destroy; override;
-  end;
-
-  { TWin32ToWin64Def }
-  // Used to keep information for TCrossInstaller descendants
-  // used in registration
-  TWin32ToWin64Def=Class(TCrossInstallerDef)
-    Class Function CrossInstallerName: String; override;
-    Class Function CrossInstallerClass: TCrossInstallerClass; override;
-    Class Function TargetCPU: String; override;
-    Class Function TargetName: String; override;
-  end;
-  TWin32ToWin64DefClass = class of TWin32ToWin64Def;
-
 implementation
+type
 
-{ TWin32ToWin64Def }
-// If using this for your own extension, modify these functions:
-class function TWin32ToWin64Def.CrossInstallerName: String;
-begin
-  Result:='Win32 to Win64 cross compiler';
-end;
+{ TWin32To64 }
 
-class function TWin32ToWin64Def.CrossInstallerClass: TCrossInstallerClass;
-begin
-  Result:=TWin32To64;
-end;
+TWin32To64 = class(TCrossInstaller)
+private
 
-class function TWin32ToWin64Def.TargetCPU: String;
-begin
-  // Target processor for cross compiler
-  // according to definitions in MAKEFILETARGETS in FPC MakeFile
-  Result:='x86_64';
-end;
+public
 
-class function TWin32ToWin64Def.TargetName: String;
-begin
-  // Target operating system for cross compiler
-  // according to definitions in MAKEFILETARGETS in FPC MakeFile
-  Result:='win64';
-end;
-
-procedure TWin32To64.SetBinUtilsPath(AValue: string);
-begin
-  if FBinUtilsPath=AValue then Exit;
-  FBinUtilsPath:=AValue;
-end;
-
-procedure TWin32To64.SetBinUtilsPrefix(AValue: string);
-begin
-  if FBinUtilsPrefix=AValue then Exit;
-  FBinUtilsPrefix:=AValue;
-end;
-
-procedure TWin32To64.SetLibsPath(AValue: string);
-begin
-  if FLibsPath=AValue then Exit;
-  FLibsPath:=AValue;
-end;
-
-procedure TWin32To64.SetTargetCPU(AValue: string);
-begin
-  if FTargetCPU=AValue then Exit;
-  FTargetCPU:=AValue;
-end;
-
-procedure TWin32To64.SetTargetOS(AValue: string);
-begin
-  if FTargetOS=AValue then Exit;
-  FTargetOS:=AValue;
-end;
-
-function TWin32To64.GetLibs: boolean;
-begin
-  // Used to get required libraries
-  // for cross compiler target and
-  // put them in the libspath directory.
-
-  // Not needed for Win64; FPC can use the win32 libs
-end;
-
-function TWin32To64.GetBinUtils: boolean;
-begin
-  // Used to get required binutils (as, ar, etc)
-  // for cross compiler target and
-  // put them in the binutilspath directory.
-
-  // Not needed for Win64; FPC can use the win32 binutils!?!
+  constructor Create;
+  destructor Destroy; override;
 end;
 
 { TWin32To64 }
+
 constructor TWin32To64.Create;
 begin
   inherited Create;
-  FBinUtilsPath:=''; //Not needed for win64
-  FBinUtilsPrefix:=''; //Not needed for win64
-  FLibsPath:=''; //Not needed for win64
-  FTargetCPU:='x86_64';
-  FTargetOS:='win64';
 end;
 
 destructor TWin32To64.Destroy;
 begin
   inherited Destroy;
 end;
-
-initialization
-  RegisterCrossInstaller(TWin32ToWin64Def);
 
 end.
 
