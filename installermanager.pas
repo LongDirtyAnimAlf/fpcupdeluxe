6f9214463a9527f9685602cdd0e7b385c7705ee6
ludob <ludob@localhost>
2012-02-25 16:09:03 +0100

    Added TSequencer to installerManager. First routines implemented

--- installermanager.pas
+++ installermanager.pas
@@ -5,7 +5,7 @@ unit installerManager;
 interface
 
 uses
-  Classes, SysUtils;
+  Classes, SysUtils,installerCore,installerFpc,installerLazarus,installerUniversal;
 
 type
 
@@ -37,6 +37,8 @@ type
     FShortCutNameFpcup: string;
     FSkipModules: string;
     FVerbose: boolean;
+  protected
+    LogFile:Text;
   public
     property ShortCutName: string read FShortCutName write FShortCutName;
     property ShortCutNameFpcup:string read FShortCutNameFpcup write FShortCutNameFpcup;
@@ -68,13 +70,76 @@ type
   end;
 
 
+  TExecState=(ESNever,ESFailed,ESSucceeded);
+
+  TSequenceAttributes=record
+    EntryPoint:integer;  //instead of rescanning the sequence table everytime, we can as well store the index in the table
+    Executed:TExecState; //  Reset to ESNever at sequencer start up
+    Standard:boolean;    //true: added by code; false: added at run-time by TUniversalInstaller
+  end;
+
+  TKeyword=(SMdeclare, SMdo, SMrequire, SMexec, SMend, SMcleanmodule, SMgetmodule, SMbuildmodule,
+    SMuninstallmodule, SMconfigmodule, SMSetLCL, SMSetOS, SMSetCPU);
+
+  TState=record
+    instr:TKeyword;
+    param:string;
+    end;
+
+  { TSequencer }
+
+  TSequencer=class(TObject)
+    protected
+      FParent:TFPCupManager;
+      CurrentModule:String;
+      Installer:TInstaller;  //current installer
+      ModuleList:TStringList;
+      SkipList:TStringList;
+      TStateMachine:array of TState;
+      function DoBuildModule(ModuleName:string):boolean;
+      function DoConfigModule(ModuleName:string):boolean;
+      function DoExec(FunctionName:string):boolean;
+      function DoGetModule(ModuleName:string):boolean;
+      function DoUnInstallModule(ModuleName:string):boolean;
+      function GetInstaller(ModuleName:string):boolean;
+      function IsSkipped(ModuleName:string):boolean;
+    public
+      property Parent:TFPCupManager write Fparent;
+      function CreateOnly(OnlyModules:string):boolean;
+      function LoadModuleList:boolean;
+      function DeleteOnly:boolean;
+      function Run(SequenceName:string):boolean;
+    end;
+
 implementation
 
 { TFPCupManager }
 
 function TFPCupManager.Run: boolean;
+var Sequencer:TSequencer;
 begin
-  result:=true; //be optimistic !!!
+  result:=false;
+  Sequencer:=TSequencer.create;
+  Sequencer.Parent:=Self;
+  try
+    if Sequencer.LoadModuleList then
+      begin
+      if FOnlyModules<>'' then
+        begin
+        Sequencer.CreateOnly(FOnlyModules);
+        result:=Sequencer.Run('Only');
+        Sequencer.DeleteOnly;
+        end
+      else
+        {$ifdef win32}
+        result:=Sequencer.Run('DefaultWin32');
+        {$else}
+        result:=Sequencer.Run('Default');
+        {$endif win32}
+      end;
+  finally
+    Sequencer.free;
+  end;
 end;
 
 constructor TFPCupManager.Create;
@@ -85,6 +150,157 @@ end;
 destructor TFPCupManager.Destroy;
 begin
   inherited Destroy;
+end;
+
+{ TSequencer }
+
+function TSequencer.DoBuildModule(ModuleName: string): boolean;
+begin
+
+end;
+
+function TSequencer.DoConfigModule(ModuleName: string): boolean;
+begin
+
+end;
+
+function TSequencer.DoExec(FunctionName: string): boolean;
+begin
+
+end;
+
+function TSequencer.DoGetModule(ModuleName: string): boolean;
+begin
+
+end;
+
+function TSequencer.DoUnInstallModule(ModuleName: string): boolean;
+begin
+
+end;
+
+{GetInstaller gets a new installer for ModuleName and initialises parameters unless one exist already.}
+
+function TSequencer.GetInstaller(ModuleName: string): boolean;
+var
+  CrossCompiling:boolean;
+begin
+  CrossCompiling:= (FParent.CrossCPU_Target<>'') or (FParent.CrossOS_Target<>'');
+  //check if this is a known module
+  if uppercase(ModuleName)='FPC' then
+    begin
+    if assigned(Installer) then
+      begin
+      if (not crosscompiling and (Installer is TFPCNativeInstaller)) or
+        ( crosscompiling and (Installer is TFPCCrossInstaller)) then
+        begin
+        result:=true; //all fine, continue with current installer
+        exit;
+        end
+      else
+        Installer.free; // get rid of old installer
+      end;
+    if not CrossCompiling then
+      begin
+      Installer:=TFPCCrossInstaller.Create;
+      Installer.CrossOS_Target:=FParent.CrossOS_Target;
+      Installer.CrossCPU_Target:=FParent.CrossCPU_Target;
+      end
+    else
+      Installer:=TFPCNativeInstaller.Create;
+    Installer.BaseDirectory:=FParent.FPCDirectory;
+    (Installer as TFPCInstaller).BootstrapCompilerDirectory:=FParent.BootstrapCompilerDirectory;
+    (Installer as TFPCInstaller).BootstrapCompilerURL:=FParent.BootstrapCompilerFTP;
+    Installer.Compiler:='';  //bootstrap used
+    Installer.CompilerOptions:=FParent.FPCOPT;
+    Installer.DesiredRevision:=FParent.FPCDesiredRevision;
+    Installer.LogFile:=FParent.LogFile;
+    {$IFDEF MSWINDOWS}
+    Installer.MakeDirectory:=FParent.MakeDirectory;
+    {$ENDIF}
+    Installer.URL:=FParent.FPCURL;
+    Installer.Verbose:=FParent.Verbose;
+    end
+  else if (uppercase(ModuleName)='LAZARUS') or (uppercase(ModuleName)='BIGIDE') then
+    begin
+    if assigned(Installer) then
+      begin
+      if (not crosscompiling and (Installer is TLazarusNativeInstaller)) or
+        ( crosscompiling and (Installer is TLazarusCrossInstaller)) then
+        begin
+        result:=true; //all fine, continue with current installer
+        exit;
+        end
+      else
+        Installer.free; // get rid of old installer
+      end;
+    if not CrossCompiling then
+      begin
+      Installer:=TLazarusCrossInstaller.Create;
+      Installer.CrossOS_Target:=FParent.CrossOS_Target;
+      Installer.CrossCPU_Target:=FParent.CrossCPU_Target;
+      end
+    else
+      Installer:=TLazarusNativeInstaller.Create;
+    Installer.BaseDirectory:=FParent.LazarusDirectory ;
+    if FParent.CompilerName='' then
+      Installer.Compiler:=Installer.GetCompilerInDir(FParent.FPCDirectory)
+    else
+      Installer.Compiler:=FParent.CompilerName;
+    Installer.CompilerOptions:=FParent.LazarusOPT;
+    Installer.DesiredRevision:=FParent.LazarusDesiredRevision;
+    Installer.LogFile:=FParent.LogFile;
+    {$IFDEF MSWINDOWS}
+    Installer.MakeDirectory:=FParent.MakeDirectory;
+    {$ENDIF}
+    Installer.URL:=FParent.FPCURL;
+    Installer.Verbose:=FParent.Verbose;
+    end
+  else if (uppercase(ModuleName)='HELP') then
+    begin
+    end
+  else       // this is a universal module
+    begin
+      if assigned(Installer) then
+        begin
+        if (Installer is TUniversalInstaller) and
+          (CurrentModule= ModuleName) then
+          begin
+          result:=true; //all fine, continue with current installer
+          exit;
+          end
+        else
+          Installer.free; // get rid of old installer
+        end;
+      Installer:=TUniversalInstaller.Create;
+      CurrentModule:=ModuleName;
+      //assign properties
+    end;
+end;
+
+function TSequencer.IsSkipped(ModuleName: string): boolean;
+begin
+
+end;
+
+function TSequencer.CreateOnly(OnlyModules: string): boolean;
+begin
+
+end;
+
+function TSequencer.LoadModuleList: boolean;
+begin
+
+end;
+
+function TSequencer.DeleteOnly: boolean;
+begin
+
+end;
+
+function TSequencer.Run(SequenceName: string): boolean;
+begin
+
 end;
 
 end.
