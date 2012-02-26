5c77c4fdbbb71f75a2158084f27c58ce4782ffb1
reiniero <reiniero@localhost>
2012-02-26 11:38:11 +0100

    Started help

--- installerhelp.pas
+++ installerhelp.pas
@@ -1,4 +1,42 @@
+{ Help installer/uninstaller unit for fpcup
+Copyright (C) 2012 Ludo Brands, Reinier Olislagers
+
+This library is free software; you can redistribute it and/or modify it
+under the terms of the GNU Library General Public License as published by
+the Free Software Foundation; either version 2 of the License, or (at your
+option) any later version with the following modification:
+
+As a special exception, the copyright holders of this library give you
+permission to link this library with independent modules to produce an
+executable, regardless of the license terms of these independent modules,and
+to copy and distribute the resulting executable under terms of your choice,
+provided that you also meet, for each linked independent module, the terms
+and conditions of the license of that module. An independent module is a
+module which is not derived from or based on this library. If you modify
+this library, you may extend this exception to your version of the library,
+but you are not obligated to do so. If you do not wish to do so, delete this
+exception statement from your version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public License
+for more details.
+
+You should have received a copy of the GNU Library General Public License
+along with this library; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+}
 unit installerHelp;
+
+{This class installs, configures and uninstalls FPC and Lazarus help.
+It is called by the state machine in installerManager.
+
+When installing, the class downloads FPC RTL/FCL/reference .CHM files,
+because compiling them from source is very complicated, and FPC help is
+fairly static.
+An LCL help CHM is generated from the Lazarus sources and cross-reference
+information in the FPC help.
+}
 
 {$mode objfpc}{$H+}
 
@@ -6,7 +44,44 @@ interface
 
 uses
   Classes, SysUtils, installerCore;
+Const
+  Sequences=
+// Convention: help modules start with help
+//FPC .CHM download
+    'Declare helpfpc;'+
+    'Cleanmodule helpfpc;'+
+    'Getmodule helpfpc;'+
+    'End;'+
+//Lazarus/FPC help
+    'Declare help;'+
+    'Requires BIGIDE;'+
+    'Requires lhelp;'+
+    // We need FPC help before building:
+    'Requires helpfpc;'+
+    'CleanModule helplazarus;'+
+    'BuildModule helplazarus;'+
+    'ConfigModule helplazarus;'+
+    'End;'+
+//selective actions triggered with --only=SequenceName
+    'Declare HelpFPCCleanOnly;'+
+    'Cleanmodule helpfpc;'+
+    'End;'+
 
+    'Declare HelpLazarusCleanOnly;'+
+    'Cleanmodule helplazarus;'+
+    'End;'+
+
+    'Declare HelpFPCGetOnly;'+
+    'Getmodule helpfpc;'+
+    'End;'+
+
+    'Declare HelpLazarusBuildOnly;'+
+    'Buildmodule helplazarus;'+
+    'End;'+
+
+    'Declare HelpLazarusConfigOnly;'+
+    'Configmodule helplazarus;'+
+    'End';
 type
 
 { THelpInstaller }
@@ -53,6 +128,7 @@ end;
 
 implementation
 
+uses fpcuputil;
 
 { THelpInstaller }
 
@@ -68,7 +144,9 @@ end;
 
 function THelpInstaller.BuildModule(ModuleName: string): boolean;
 begin
-
+  if not InitModule then exit;
+  // We pass all responsibility to specialized THelpInstaller descendants.
+  result:=BuildModuleCustom(ModuleName);
 end;
 
 function THelpInstaller.CleanModule(ModuleName: string): boolean;
@@ -77,8 +155,61 @@ begin
 end;
 
 function THelpInstaller.GetModule(ModuleName: string): boolean;
+var
+  OperationSucceeded: boolean;
+  ResultCode: longint;
+  DocsZip: string;
 begin
+  if not InitModule then exit;
+  OperationSucceeded:=false;
+  //todo: check if this is the right way to specify that fpc help
+  //must be used
+  if UpperCase(ModuleName)='HELPFPC' then
+  begin
+    if (Self is THelpFPCInstaller)=false then
+    begin
+      writelnlog('Don''t know how to get module '+ModuleName,true);
+      OperationSucceeded:=false;
+    end
+    else
+    begin
+      // Download FPC CHM docs zip into TargetDirectory.
+      OperationSucceeded:=true;
+      ForceDirectories(TargetDirectory);
+      DocsZip := SysUtils.GetTempFileName + '.zip';
+      try
+        OperationSucceeded:=Download(URL,DocsZip);
+      except
+        on E: Exception do
+        begin
+          // Deal with timeouts, wrong URLs etc
+          OperationSucceeded:=false;
+          infoln('DownloadFPCHelp: HTTP download failed. URL: '+URL+LineEnding+
+            'Exception: '+E.ClassName+'/'+E.Message);
+        end;
+      end;
 
+      if OperationSucceeded then
+      begin
+        // Extract, overwrite, flatten path/junk paths
+        // todo: test with spaces in path
+        if ExecuteCommandHidden(FUnzip,'-o -j -d '+IncludeTrailingPathDelimiter(TargetDirectory)+' '+DocsZip,Verbose)= 0 then
+        begin
+          SysUtils.deletefile(DocsZip); //Get rid of temp zip if success.
+        end
+        else
+        begin
+          OperationSucceeded := False;
+          infoln('DownloadFPCHelp: unzip failed with resultcode: '+IntToStr(ResultCode));
+        end;
+      end
+      else
+      begin
+        infoln('DownloadFPCHelp: HTTP download failed. URL: '+URL);
+      end;
+    end;
+  end;
+  Result := OperationSucceeded;
 end;
 
 function THelpInstaller.UnInstallModule(ModuleName: string): boolean;
