39e4efab942c8f7d5d274d24dfb88f93607ae138
ludob <ludob@localhost>
2012-02-26 15:14:47 +0100

    Repaired accidental overwrite of previous commits

--- installermanager.pas
+++ installermanager.pas
@@ -5,7 +5,7 @@ unit installerManager;
 interface
 
 uses
-  Classes, SysUtils,installerCore,installerFpc,installerLazarus,installerUniversal,fpcuputil,fileutil;
+  Classes, SysUtils,installerCore,installerFpc,installerLazarus,installerHelp,installerUniversal,fpcuputil,fileutil;
 
 Const
   Sequences=
@@ -452,9 +452,55 @@ begin
     Installer.URL:=FParent.FPCURL;
     Installer.Verbose:=FParent.Verbose;
     end
-  else if (uppercase(ModuleName)='HELP') then
-    begin
-    end
+  //Convention: help modules start with HelpFPC
+  //or HelpLazarus
+  else if AnsiPos('HELPFPC', uppercase(ModuleName))=1 then
+      begin
+      if assigned(Installer) then
+        begin
+        if (Installer is THelpFPCInstaller) then
+          begin
+          result:=true; //all fine, continue with current installer
+          exit;
+          end
+        else
+          Installer.free; // get rid of old installer
+        end;
+      Installer:=THelpFPCInstaller.Create;
+      Installer.BaseDirectory:=FParent.FPCDirectory;
+      if FParent.CompilerName='' then
+        Installer.Compiler:=Installer.GetCompilerInDir(FParent.FPCDirectory)
+      else
+        Installer.Compiler:=FParent.CompilerName;
+      Installer.LogFile:=FParent.LogFile;
+      {$IFDEF MSWINDOWS}
+      Installer.MakeDirectory:=FParent.MakeDirectory;
+      {$ENDIF}
+      end
+  else if AnsiPos('HELPLAZARUS', uppercase(ModuleName))=1 then
+      begin
+      if assigned(Installer) then
+        begin
+       if (Installer is THelpLazarusInstaller) then
+          begin
+          result:=true; //all fine, continue with current installer
+          exit;
+          end
+        else
+          Installer.free; // get rid of old installer
+        end;
+      Installer:=THelpLazarusInstaller.Create;
+      Installer.BaseDirectory:=FParent.LazarusDirectory ;
+      if FParent.CompilerName='' then
+        Installer.Compiler:=Installer.GetCompilerInDir(FParent.FPCDirectory)
+      else
+        Installer.Compiler:=FParent.CompilerName;
+      (Installer as THelpLazarusInstaller).LazarusPrimaryConfigPath:=FParent.LazarusPrimaryConfigPath;
+      Installer.LogFile:=FParent.LogFile;
+      {$IFDEF MSWINDOWS}
+      Installer.MakeDirectory:=FParent.MakeDirectory;
+      {$ENDIF}
+      end
   else       // this is a universal module
     begin
       if assigned(Installer) then
