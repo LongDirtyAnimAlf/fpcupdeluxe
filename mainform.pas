d9b57ace3980255c05a637bb0c9b428115feff09
reiniero <reiniero@localhost>
2014-01-18 15:49:36 +0100

    GUI: remove settings; use .ini file/profile for that.

--- mainform.pas
+++ mainform.pas
@@ -5,8 +5,9 @@ unit mainform;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  EditBtn, ComCtrls, ExtCtrls, ValEdit, installermanager;
+  Classes, SysUtils, FileUtil, SynMemo, SynHighlighterIni, Forms, Controls,
+  Graphics, Dialogs, StdCtrls, EditBtn, ComCtrls, ExtCtrls, ValEdit,
+  inifiles;
 
 type
 
@@ -14,57 +15,27 @@ type
 
   TForm1 = class(TForm)
     CommandMemo: TMemo;
-    FPCBootstrapDir: TDirectoryEdit;
-    Label13: TLabel;
+    FileNameEdit: TFileNameEdit;
+    INIFileLabel: TLabel;
     OutputMemo: TMemo;
-    SkipFPC: TCheckBox;
-    SkipLazarus: TCheckBox;
-    Label11: TLabel;
-    Label12: TLabel;
-    LazarusPrimaryConfigPath: TDirectoryEdit;
-    LazarusOptionsGroup: TGroupBox;
-    BinutilsDir: TDirectoryEdit;
-    LazarusDirectory: TDirectoryEdit;
-    FPCOptions: TEdit;
-    FPCUpScriptName: TEdit;
-    LazarusRevision: TEdit;
-    LazarusOptions: TEdit;
-    FPCURL: TComboBox;
-    LazarusLinkName: TEdit;
-    LazarusURL: TComboBox;
-    GeneralOptionsGroup: TGroupBox;
-    HeadButton: TButton;
-    FPCRevision: TEdit;
-    LazarusHeadButton: TButton;
-    Label10: TLabel;
-    Label3: TLabel;
-    Label4: TLabel;
-    Label5: TLabel;
-    BinutilsDirLabel: TLabel;
-    Label7: TLabel;
-    Label8: TLabel;
-    Label9: TLabel;
-    ListBox1: TListBox;
-    LazarusOptionsChoices: TListBox;
-    NoFPCUPScriptButton: TButton;
     OutputTabs: TPageControl;
     OutputTab: TTabSheet;
     CommandTab: TTabSheet;
+    EditTabs: TPageControl;
+    OptionsTab: TTabSheet;
+    IniEditorTab: TTabSheet;
+    ProfileLabel: TLabel;
+    ProfileSelect: TComboBox;
+    SynIniHighlighter: TSynIniSyn;
+    SynMemo: TSynMemo;
     UpdateButton: TButton;
-    FPCDirectory: TDirectoryEdit;
-    Label2: TLabel;
-    FPCOptionsGroup: TGroupBox;
-    Label1: TLabel;
+    procedure FileNameEditAcceptFileName(Sender: TObject; var Value: String);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
-    procedure HeadButtonClick(Sender: TObject);
-    procedure LazarusHeadButtonClick(Sender: TObject);
-    procedure NoFPCUPScriptButtonClick(Sender: TObject);
     procedure UpdateButtonClick(Sender: TObject);
   private
     { private declarations }
     FFPCUPParams: TStringList;
-    FManager: TFPCupManager;
     procedure UpdateCommand;
   public
     { public declarations }
@@ -77,10 +48,6 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.NoFPCUPScriptButtonClick(Sender: TObject);
-begin
-  FPCUpScriptName.Text:=EmptyStr;
-end;
 
 procedure TForm1.UpdateButtonClick(Sender: TObject);
 begin
@@ -97,41 +64,42 @@ begin
   CommandMemo.Text:='fpcup '+FFPCUpParams.DelimitedText; //Show how fpcup cli would be invoked
 end;
 
-procedure TForm1.HeadButtonClick(Sender: TObject);
-begin
-  FPCRevision.Text:='HEAD';
-end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
   FFPCUpParams:=TStringList.Create;
   FFPCUpParams.Delimiter:=' ';
   FFPCUpParams.StrictDelimiter:=false; //I believe this works best for output
-  FManager:=TFPCupManager.Create;
+end;
 
-  LazarusPrimaryConfigPath.Directory:=FManager.LazarusPrimaryConfigPath; //Get a default primary config path
-  // Set up defaults depending on platform
-  {$IFDEF WINDOWS}
-  //Defaults already set up in design mode GUI or in code above
-  {$ENDIF WINDOWS}
-  {$IFDEF UNIX}
-  BinutilsDir.Visible:=false; //No use on Unix
-  FPCBootstrapDir.Directory:='~/fpcbootstrap';
-  FPCDirectory.Directory:='~/fpc';
-  LazarusDirectory.Directory:='~/lazarus';
-  {$ENDIF UNIX}
+procedure TForm1.FileNameEditAcceptFileName(Sender: TObject; var Value: String);
+var
+  MyIniFile : TIniFile;
+  Sections: TStringList;
+begin
+  // Load selected ini file
+  SynMemo.BeginUpdate(false);
+  SynMemo.Lines.LoadFromFile(Value);
+  MyIniFile:=TIniFile.Create(Value,true);
+  Sections:=TStringList.Create;
+  try
+    Sections.Duplicates:=dupIgnore; //just pick the first one or whatever
+    Sections.Sorted:=true;
+    MyIniFile.ReadSections(Sections);
+    ProfileSelect.Clear;
+    ProfileSelect.Items.AddStrings(Sections);
+  finally
+    SynMemo.EndUpdate;
+    Sections.Free;
+    MyIniFile.Free;
+  end;
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
 begin
   FFPCUpParams.Free;
-  FManager.Free;
 end;
 
-procedure TForm1.LazarusHeadButtonClick(Sender: TObject);
-begin
-  LazarusRevision.Text:='HEAD';
-end;
 
 {$R *.lfm}
 
