b82c742c052cd3d3a99ed7305a6fa9e56099a2a5
reiniero <reiniero@localhost>
2014-09-08 15:25:10 +0200

    debugging

--- updatelazconfig.pas
+++ updatelazconfig.pas
@@ -613,8 +613,8 @@ begin
     raise Exception.Create('TUpdateLazConfig: ConfigPath may not be empty');
   FDefaultConfigPath:=IncludeTrailingPathDelimiter(SafeExpandFileName(ConfigPath));
   //todo: debug
-  infoln('todo: debug: tupdatelazonfig.create: got configpath:   '+ConfigPath);
-  infoln('todo: debug: tupdatelazonfig.create: got def confpath: '+FDefaultConfigPath);
+  infoln('todo: debug: tupdatelazonfig.create: got configpath:   '+ConfigPath,etInfo);
+  infoln('todo: debug: tupdatelazonfig.create: got def confpath: '+FDefaultConfigPath,etInfo);
 end;
 
 destructor TUpdateLazConfig.Destroy;
