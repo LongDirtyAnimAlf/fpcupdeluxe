c99d370988eb7387f750edf2e1462218362597d3
LongDirtyAnimAlf <LongDirtyAnimAlf@gmail.com>
2020-10-31 09:52:43 +0100

    Two more files for zbar

---
+++ variousbuildscripts/android/zbar-android/jni/java/net/sourceforge/zbar/zbar-build.bat
@@ -0,0 +1,22 @@
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Android libiconv buildscript for Windows
+@rem  Please set the path towards the NDK
+@rem
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set java=C:\Program Files\Java\jdk1.8.0_271\bin
+
+set javac=%java%\javac.exe
+set jar=%java%\jar.exe
+
+call "%javac%" *.java
+call "%jar%" cf zbar.jar *.class
+
+if "%OS%"=="Windows_NT" endlocal
+
+pause
