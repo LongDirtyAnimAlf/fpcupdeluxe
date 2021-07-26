9d8f254297af663400392067ee974eeaa0616f6f
Don <LongDirtyAnimAlf@gmail.com>
2021-07-26 10:27:01 +0100

    Small GUI tweaks.

--- fpcupdeluxemainform.pas
+++ fpcupdeluxemainform.pas
@@ -4229,6 +4229,7 @@ begin
   AddMessage('');
 
   try
+    {$ifdef READER}
     try
       //Form1.SetFocusedControl(BitBtnHalt);
       CommandOutputScreen.Enabled:=false;
@@ -4237,6 +4238,9 @@ begin
       //Form1.SetFocusedControl(btnInstallDirSelect);
       CommandOutputScreen.Enabled:=true;
     end;
+    {$else}
+    result:=FPCupManager.Run;
+    {$endif}
     if (NOT result) then
     begin
       AddMessage('');
@@ -4758,30 +4762,36 @@ begin
 
   if (ExistWordInString(PChar(MsgPasStr),'failed to get crossbinutils',[soDown])) then
   begin
-    memoSummary.Lines.Append('Missing correct cross libraries');
+    if (NOT MissingCrossBins) then memoSummary.Lines.Append('Missing correct cross libraries');
     MissingCrossBins:=true;
   end
   else if (ExistWordInString(PChar(MsgPasStr),'failed to get crosslibrary',[soDown])) then
   begin
-    memoSummary.Lines.Append('Missing correct cross libraries');
+    if (NOT MissingCrossLibs) then memoSummary.Lines.Append('Missing correct cross libraries');
     MissingCrossLibs:=true;
   end
   else if ((ExistWordInString(PChar(MsgPasStr),'CheckAndGetTools',[soDown])) OR (ExistWordInString(PChar(MsgPasStr),'Required package is not installed',[soDown]))) then
   begin
+    if (NOT MissingTools) then
+    begin
+      {$ifdef Darwin}
+      memoSummary.Lines.Append('Missing some tools: please install Xcode command line tools !');
+      memoSummary.Lines.Append('xcode-select --install');
+      {$else}
+      memoSummary.Lines.Append('Missing some tools: please install missing tools!');
+      {$endif}
+    end;
     MissingTools:=true;
-    {$ifdef Darwin}
-    memoSummary.Lines.Append('Missing some tools: please install Xcode command line tools !');
-    memoSummary.Lines.Append('xcode-select --install');
-    {$else}
-    memoSummary.Lines.Append('Missing some tools: please install missing tools!');
-    {$endif}
   {$ifdef Darwin}
   end
   else if (ExistWordInString(PChar(MsgPasStr),'The subversion command line tools are no longer provided by Xcode',[soDown])) then
   begin
+    if (NOT MissingTools) then
+    begin
+      memoSummary.Lines.Append('SVN is no longer included in Xcode command line tools !');
+      memoSummary.Lines.Append('Use a GIT repo (preferred) or install SVN by yourself (brew).');
+    end;
     MissingTools:=true;
-    memoSummary.Lines.Append('SVN is no longer included in Xcode command line tools !');
-    memoSummary.Lines.Append('Use a GIT repo (preferred) or install SVN by yourself (brew).');
   {$endif}
   end;
 
