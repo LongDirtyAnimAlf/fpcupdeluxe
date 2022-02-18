6065c67659a0c8b6844928eec09193f794111e56
Don <LongDirtyAnimAlf@gmail.com>
2022-02-18 09:05:30 +0000

    Oracle install script

---
+++ installscripts/oracle.sh
@@ -0,0 +1,18 @@
+sudo yum install wget
+sudo yum install make
+sudo yum install gcc
+sudo yum install gdb
+sudo yum install glib
+sudo yum install subversion
+sudo yum install git
+sudo yum install binutils
+
+sudo yum install zip
+sudo yum install unzip
+sudo yum install p7zip
+sudo yum install xterm
+
+sudo yum install gtk2-devel
+sudo yum install gdk-pixbuf2-devel
+sudo yum install pango-devel
+sudo yum install glibc-devel
